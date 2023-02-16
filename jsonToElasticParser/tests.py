031cfd0dfb1c4a3719f974a1387d678da29affe7
maros Studenic <studenic.maros@gmail.com>
2023-02-16 01:27:38 +0100

    import tests to be testable by ./manage.py test

--- jsonToElasticParser/tests.py
+++ jsonToElasticParser/tests.py
@@ -1,3 +1,4 @@
 from django.test import TestCase
-
 # Create your tests here.
+from jsonToElasticParser.endpoints_tests.name_endpoint import ModelNameTestCase
+from jsonToElasticParser.endpoints_tests.name_id_endpoint import ModelNameIdTestCase
