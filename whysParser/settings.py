d001aaf1d466a36e32ebcefb200b2d2326dab833
maros Studenic <studenic.maros@gmail.com>
2023-02-16 03:39:29 +0100

    fix secret_key leak

--- whysParser/settings.py
+++ whysParser/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z$5_nulgy2yn6)@^bwqlm^uh9u2s_#$gl66$z6(^3oqwt$u4!k'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
